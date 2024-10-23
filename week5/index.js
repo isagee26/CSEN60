a6afea7d51a108ef7105af99957b9d8e4da96cee
isa gee <isagee@isas-MacBook-Air.local>
2024-10-23 11:35:05 -0700

    javascript operators

--- week5/index.js
+++ week5/index.js
@@ -1,6 +1,6 @@
 //console.log("hello world")
 //alert('Hello, world!');
-window.document.write('<p>Welcome to CSEN 60!</p>');
+// window.document.write('<p>Welcome to CSEN 60!</p>');
 
 // document.append('<p> This should come after h1 </p>')
 
@@ -21,3 +21,51 @@ window.document.write('<p>Welcome to CSEN 60!</p>');
 function add(a, b) {
     return a + b;
 }
+
+let x = 100;
+// let x => declaration
+// x=2 => initialization
+if (true) {
+     x = 200; // Same variable!
+}
+console.log(x); // Outputs: 200
+
+let y
+y = null
+let z = 0
+console.log(y,z)
+
+let greeting = "Hello, world!";
+let response = `The time is ${new Date().getHours()}`;
+
+console.log(response)
+
+const age = 20 {
+    if (age>18)
+        console.log("you are of age")
+}   else {
+        console.log(`you are not of age. Please wait ${18-age} years`)
+}
+
+let isAvailable = true;
+let isOverAge = (age > 18);
+
+console.log("\n", isOverAge)
+
+let person = {
+    name: "John",
+    age: 30,
+    isStudent: false
+    };
+
+console.log(person["age"])
+
+console.log(typeof("hello world"))
+
+alert("This is a important message")
+
+// Strict equality
+console.log(3 === '3'); // false
+
+// Loose equality
+console.log(3 == '3'); // true
